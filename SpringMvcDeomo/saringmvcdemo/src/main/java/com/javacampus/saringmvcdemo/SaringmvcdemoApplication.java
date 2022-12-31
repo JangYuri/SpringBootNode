8c0bea6723e572877dfae1076bbf272626f6c573
Yuri Jang <cyrjang21@gmail.com>
2022-12-31 19:37:00 +0900

    Spring MVC Demo Project Add

---
+++ SpringMvcDeomo/saringmvcdemo/src/main/java/com/javacampus/saringmvcdemo/SaringmvcdemoApplication.java
@@ -0,0 +1,13 @@
+package com.javacampus.saringmvcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaringmvcdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaringmvcdemoApplication.class, args);
+	}
+
+}
---
+++ SpringMvcDeomo/saringmvcdemo/src/test/java/com/javacampus/saringmvcdemo/SaringmvcdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacampus.saringmvcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaringmvcdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
